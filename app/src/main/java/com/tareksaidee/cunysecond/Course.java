6121bcaa7b7b18bd6979133024cfc32dc8cbe736
Tarek <tareksaidee@hotmail.com>
2018-02-15 00:41:11 -0500

    create course class

---
+++ app/src/main/java/com/tareksaidee/cunysecond/Course.java
@@ -0,0 +1,192 @@
+package com.tareksaidee.cunysecond;
+
+import java.time.LocalTime;
+import java.util.List;
+
+/**
+ * Created by tarek on 2/15/2018.
+ */
+
+public class Course {
+
+    private String name;
+    private int sectionID;
+    private String room;
+    private int capacity;
+    private int enrolled;
+    private int credits;
+    private String department;
+    private String classID;
+    private List<String> days;
+    private String school;
+    private String instructor;
+    private String level;
+    private String semester;
+    private int year;
+    private String mode;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private List<String> prereqs;
+    private String description;
+    private List<String> textbooks;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSectionID() {
+        return sectionID;
+    }
+
+    public void setSectionID(int sectionID) {
+        this.sectionID = sectionID;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getEnrolled() {
+        return enrolled;
+    }
+
+    public void setEnrolled(int enrolled) {
+        this.enrolled = enrolled;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getClassID() {
+        return classID;
+    }
+
+    public void setClassID(String classID) {
+        this.classID = classID;
+    }
+
+    public List<String> getDays() {
+        return days;
+    }
+
+    public void setDays(List<String> days) {
+        this.days = days;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getSemester() {
+        return semester;
+    }
+
+    public void setSemester(String semester) {
+        this.semester = semester;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public List<String> getPrereqs() {
+        return prereqs;
+    }
+
+    public void setPrereqs(List<String> prereqs) {
+        this.prereqs = prereqs;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getTextbooks() {
+        return textbooks;
+    }
+
+    public void setTextbooks(List<String> textbooks) {
+        this.textbooks = textbooks;
+    }
+}
