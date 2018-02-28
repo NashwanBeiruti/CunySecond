0e2322e0c835b2a310e3e6ce1a1e52a47de5c59a
Tarek <tareksaidee@hotmail.com>
2018-02-27 23:50:14 -0500

    Add more classes

--- app/src/main/java/com/tareksaidee/cunysecond/Student.java
+++ app/src/main/java/com/tareksaidee/cunysecond/Student.java
@@ -116,62 +116,6 @@ public class Student {
         this.phoneNumber = phoneNumber;
     }
 
-    private class MiniCourse {
-        private String name;
-        private String startTime;
-        private String endTime;
-        private int credits;
-        private String room;
-        private String grade;
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getStartTime() {
-            return startTime;
-        }
-
-        public void setStartTime(String startTime) {
-            this.startTime = startTime;
-        }
-
-        public String getEndTime() {
-            return endTime;
-        }
-
-        public void setEndTime(String endTime) {
-            this.endTime = endTime;
-        }
-
-        public int getCredits() {
-            return credits;
-        }
-
-        public void setCredits(int credits) {
-            this.credits = credits;
-        }
-
-        public String getRoom() {
-            return room;
-        }
-
-        public void setRoom(String room) {
-            this.room = room;
-        }
-
-        public String getGrade() {
-            return grade;
-        }
-
-        public void setGrade(String grade) {
-            this.grade = grade;
-        }
-    }
 }
 
 
