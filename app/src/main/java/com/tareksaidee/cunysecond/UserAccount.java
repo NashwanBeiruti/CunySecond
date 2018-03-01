65050aad962d2abea3029d2d94230e61d96e052b
Tarek <tareksaidee@hotmail.com>
2018-03-01 14:20:37 -0500

    hook user account to db

--- app/src/main/java/com/tareksaidee/cunysecond/UserAccount.java
+++ app/src/main/java/com/tareksaidee/cunysecond/UserAccount.java
@@ -1,13 +1,83 @@
 package com.tareksaidee.cunysecond;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class UserAccount extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;
+    private String userUID;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mUserDatabaseReference;
+    private ChildEventListener mChildEventListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_account);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        userUID = mFirebaseAuth.getCurrentUser().getUid();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mUserDatabaseReference = mFirebaseDatabase.getReference().child("users");
+    }
+
+    void attachDatabaseReadListener() {
+        if (mChildEventListener == null) {
+            mChildEventListener = new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                    Student student = dataSnapshot.getValue(Student.class);
+
+                }
+
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            };
+            mUserDatabaseReference.orderByKey().equalTo(userUID).addChildEventListener(mChildEventListener);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        detachReadListener();
+        //coursesAdapter.clear();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        attachDatabaseReadListener();
+    }
+
+    void detachReadListener() {
+        if (mChildEventListener != null) {
+            mUserDatabaseReference.removeEventListener(mChildEventListener);
+            mChildEventListener = null;
+        }
     }
 }
