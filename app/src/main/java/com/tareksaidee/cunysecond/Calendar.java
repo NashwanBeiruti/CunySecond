53c0db741d63d8aeb8ffac745de9ec2cf57f1817
Tarek <tareksaidee@hotmail.com>
2018-03-11 04:02:16 -0400

    add logic for events

--- app/src/main/java/com/tareksaidee/cunysecond/Calendar.java
+++ app/src/main/java/com/tareksaidee/cunysecond/Calendar.java
@@ -2,8 +2,7 @@ package com.tareksaidee.cunysecond;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MotionEvent;
-import android.view.View;
+import android.util.Log;
 
 import com.alamkanak.weekview.MonthLoader;
 import com.alamkanak.weekview.WeekView;
@@ -15,13 +14,17 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
 import java.util.ArrayList;
-import java.util.GregorianCalendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
-public class Calendar extends AppCompatActivity implements  MonthLoader.MonthChangeListener{
+public class Calendar extends AppCompatActivity implements MonthLoader.MonthChangeListener {
 
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference mCoursesDatabaseReference;
@@ -29,29 +32,36 @@ public class Calendar extends AppCompatActivity implements  MonthLoader.MonthCha
     private FirebaseAuth mAuthInstance;
     private WeekView mWeekView;
     private List<WeekViewEvent> events = new ArrayList<WeekViewEvent>();
+    private Map<String, List<WeekViewEvent>> mEventsMap;
     DateTimeFormatter timeFormatter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        mEventsMap = new HashMap<>();
         mAuthInstance = FirebaseAuth.getInstance();
-        timeFormatter  = DateTimeFormatter.ofPattern("hh:mm a");
+        timeFormatter = DateTimeFormatter.ofPattern("h:mm a");
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mCoursesDatabaseReference = mFirebaseDatabase.getReference().child("users")
                 .child(mAuthInstance.getCurrentUser().getUid()).child("currentCourses");
         mWeekView = (WeekView) findViewById(R.id.weekView);
         mWeekView.setMonthChangeListener(this);
-        mWeekView.goToDate(new GregorianCalendar(2018,9,22));
         //mWeekView.setXScrollingSpeed(0);
-
-
     }
 
     @Override
     public List<? extends WeekViewEvent> onMonthChange(int newYear, int newMonth) {
-        WeekViewEvent event = new WeekViewEvent(123,"Class",2018,4,10,1,0,2018,4,10,3,0);
-        events.add(event);
+        LocalDate localDate = LocalDate.of(newYear, newMonth, 1);
+        while (localDate.getMonthValue() == newMonth) {
+            String today = localDate.getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.US).toLowerCase();
+            if (mEventsMap.containsKey(today)) {
+                Log.e("check","does");
+                events.addAll(mEventsMap.get(today));
+            }
+            localDate = localDate.plusDays(1);
+        }
+        Log.e("dsadsada", events.size() + "");
         return events;
     }
 
@@ -60,11 +70,21 @@ public class Calendar extends AppCompatActivity implements  MonthLoader.MonthCha
             mChildEventListener = new ChildEventListener() {
                 @Override
                 public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-//                    MiniCourse miniCourse = dataSnapshot.getValue(MiniCourse.class);
-//                    LocalTime startTime = LocalTime.parse(miniCourse.getStartTime(),timeFormatter);
-//                    WeekViewEvent event = new WeekViewEvent(miniCourse.getSectionID(),miniCourse.getName(),
-//                            miniCourse.getYear(),1,28,startTime.getHour(),startTime.getMinute(),miniCourse.getYear(),
-//                            1,28,)
+                    MiniCourse miniCourse = dataSnapshot.getValue(MiniCourse.class);
+                    LocalTime startTime = LocalTime.parse(miniCourse.getStartTime(), timeFormatter);
+                    LocalTime endTime = LocalTime.parse(miniCourse.getEndTime(), timeFormatter);
+                    WeekViewEvent event = new WeekViewEvent(miniCourse.getSectionID(), miniCourse.getName(),
+                            miniCourse.getYear(), 1, 1, startTime.getHour(), startTime.getMinute(), miniCourse.getYear(),
+                            1, 1, endTime.getHour(), endTime.getMinute());
+                    ArrayList<String> days = (ArrayList<String>) miniCourse.getDays();
+                    for (String day : days) {
+                        if (!mEventsMap.containsKey(day)) {
+                            mEventsMap.put(day, new ArrayList<WeekViewEvent>());
+                        }
+                        mEventsMap.get(day).add(event);
+                    }
+                    mWeekView.notifyDatasetChanged();
+                    Log.e("notify","notify");
                 }
 
                 @Override
