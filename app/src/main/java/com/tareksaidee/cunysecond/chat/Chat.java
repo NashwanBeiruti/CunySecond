0e57ca3923fb3c07a5bf26badc1e5ed8cf415d2b
Tarek <tareksaidee@hotmail.com>
2018-03-12 23:55:15 -0400

    add chatting

---
+++ app/src/main/java/com/tareksaidee/cunysecond/chat/Chat.java
@@ -0,0 +1,202 @@
+package com.tareksaidee.cunysecond.chat;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+public class Chat extends AppCompatActivity {
+
+
+    private static final int RC_PHOTO_PICKER = 2;
+    public static final int DEFAULT_MSG_LENGTH_LIMIT = 400;
+
+
+    String roomName;
+    String mUsername;
+    String mUserPhoto;
+
+    private EditText mMessageEditText;
+    private Button mSendButton;
+    private ImageButton mPhotoPickerButton;
+    private RecyclerView messagesView;
+    private LinearLayoutManager layoutManager;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mMessagesDatabaseReference;
+    private ChildEventListener mChildEventListener;
+    private FirebaseStorage mFirebaseStorage;
+    private StorageReference mChatPhotosStorageReference;
+    private ChatAdapter mChatAdapter;
+    private ProgressBar mProgressBar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        mProgressBar.setVisibility(ProgressBar.VISIBLE);
+        roomName = getIntent().getStringExtra("room");
+        mUsername = getIntent().getStringExtra("username");
+        setTitle(roomName);
+        mUserPhoto = FirebaseAuth.getInstance().getCurrentUser().getPhotoUrl().toString();
+        messagesView = (RecyclerView) findViewById(R.id.messageRecyclerView);
+        mMessageEditText = (EditText) findViewById(R.id.messageEditText);
+        mPhotoPickerButton = (ImageButton) findViewById(R.id.photoPickerButton);
+        mSendButton = (Button) findViewById(R.id.sendButton);
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mMessagesDatabaseReference = mFirebaseDatabase.getReference().child(roomName);
+        mFirebaseStorage = FirebaseStorage.getInstance();
+        mChatPhotosStorageReference = mFirebaseStorage.getReference().child(roomName + "_photos");
+        mChatAdapter = new ChatAdapter(this);
+        layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        messagesView.setLayoutManager(layoutManager);
+        messagesView.setAdapter(mChatAdapter);
+        mMessageEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.toString().trim().length() > 0) {
+                    mSendButton.setEnabled(true);
+                } else {
+                    mSendButton.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+        mMessageEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(DEFAULT_MSG_LENGTH_LIMIT)});
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: Send messages on click
+                ChatMessage friendlyMessage = new ChatMessage(mMessageEditText.getText().toString(),
+                        mUsername, null, mUserPhoto);
+                mMessagesDatabaseReference.push().setValue(friendlyMessage);
+                // Clear input box
+                mMessageEditText.setText("");
+            }
+        });
+        mPhotoPickerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/jpeg");
+                intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+                startActivityForResult(Intent.createChooser(intent, "Complete action using"), RC_PHOTO_PICKER);
+            }
+        });
+    }
+
+    void attachDatabaseReadListener() {
+        if (mChildEventListener == null) {
+            mChildEventListener = new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                    ChatMessage temp = dataSnapshot.getValue(ChatMessage.class);
+                    mChatAdapter.add(temp);
+                    layoutManager.smoothScrollToPosition(messagesView,null,mChatAdapter.getItemCount()-1);
+                    mProgressBar.setVisibility(View.INVISIBLE);
+                }
+
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            };
+            mMessagesDatabaseReference.addChildEventListener(mChildEventListener);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        detachReadListener();
+        mChatAdapter.clear();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        attachDatabaseReadListener();
+    }
+
+    void detachReadListener() {
+        if (mChildEventListener != null) {
+            mMessagesDatabaseReference.removeEventListener(mChildEventListener);
+            mChildEventListener = null;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_PHOTO_PICKER && resultCode == RESULT_OK) {
+            Uri selectedImageURI = data.getData();
+            StorageReference photoRef =
+                    mChatPhotosStorageReference.child(selectedImageURI.getLastPathSegment());
+            UploadTask uploadTask = photoRef.putFile(selectedImageURI);
+            // Register observers to listen for when the download is done or if it fails
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle unsuccessful uploads
+                    Log.e("error", exception.getLocalizedMessage());
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                    @SuppressWarnings("VisibleForTests") Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    ChatMessage friendlyMessage = new ChatMessage(null, mUsername, downloadUrl.toString(), mUserPhoto);
+                    mMessagesDatabaseReference.push().setValue(friendlyMessage);
+                }
+            });
+
+        }
+    }
+}
