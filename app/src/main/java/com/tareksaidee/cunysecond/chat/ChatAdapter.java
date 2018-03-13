b580fe75c34b3b44493e6e35d946cd9cffdb00cc
Tarek <tareksaidee@hotmail.com>
2018-03-12 23:54:45 -0400

    add chat adapter

---
+++ app/src/main/java/com/tareksaidee/cunysecond/chat/ChatAdapter.java
@@ -0,0 +1,97 @@
+package com.tareksaidee.cunysecond.chat;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.github.chrisbanes.photoview.PhotoView;
+import com.makeramen.roundedimageview.RoundedImageView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by tarek on 5/30/2017.
+ */
+
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder> {
+
+    ArrayList<ChatMessage> messages;
+    private Context mContext;
+
+    ChatAdapter(@NonNull Context context) {
+        mContext = context;
+        messages = new ArrayList<>();
+
+    }
+
+    @Override
+    public ChatAdapter.ChatViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.message_item, parent, false);
+        return new ChatViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ChatAdapter.ChatViewHolder holder, int position) {
+        ChatMessage message = messages.get(position);
+        String name = message.getName();
+        String text = message.getText();
+        holder.userName.setText(name);
+        if (message.getUserPhoto() != null) {
+            Glide.with(holder.userImage.getContext())
+                    .load(message.getUserPhoto())
+                    .into(holder.userImage);
+        } else
+            Glide.with(holder.userImage.getContext())
+                    .load(R.drawable.com_facebook_profile_picture_blank_square)
+                    .into(holder.userImage);
+        boolean isPhoto = message.getPhotoUrl() != null;
+        if (isPhoto) {
+            holder.messageText.setVisibility(View.GONE);
+            holder.photoImageView.setVisibility(View.VISIBLE);
+            Glide.with(holder.photoImageView.getContext())
+                    .load(message.getPhotoUrl())
+                    .into(holder.photoImageView);
+        } else {
+            holder.messageText.setVisibility(View.VISIBLE);
+            holder.photoImageView.setVisibility(View.GONE);
+            holder.messageText.setText(text);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return messages.size();
+    }
+
+    void add(ChatMessage message) {
+        messages.add(message);
+        notifyDataSetChanged();
+    }
+
+    void clear() {
+        messages.clear();
+    }
+
+    class ChatViewHolder extends RecyclerView.ViewHolder {
+
+        TextView userName;
+        TextView messageText;
+        PhotoView photoImageView;
+        RoundedImageView userImage;
+
+        ChatViewHolder(View view) {
+            super(view);
+            userName = (TextView) view.findViewById(R.id.nameTextView);
+            messageText = (TextView) view.findViewById(R.id.messageTextView);
+            photoImageView = (PhotoView) view.findViewById(R.id.photoImageView);
+            userImage = (RoundedImageView) view.findViewById(R.id.user_image);
+        }
+    }
+
+
+}
