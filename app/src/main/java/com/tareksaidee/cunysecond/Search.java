c83cce6bcdac5138ab2eb3f125ad674ee26bff6b
Tarek <tareksaidee@hotmail.com>
2018-05-03 23:25:35 -0400

    update searching

--- app/src/main/java/com/tareksaidee/cunysecond/Search.java
+++ app/src/main/java/com/tareksaidee/cunysecond/Search.java
@@ -4,18 +4,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 public class Search extends AppCompatActivity {
 
-    EditText classID;
+    private EditText classID;
+    private Spinner schoolsSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         classID = (EditText) findViewById(R.id.class_id_field);
-
+        schoolsSpinner = (Spinner) findViewById(R.id.school_drop_down);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.schools, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        schoolsSpinner.setAdapter(adapter);
+        schoolsSpinner.setSelection(3);
     }
 
     public void startSearch(View view) {
